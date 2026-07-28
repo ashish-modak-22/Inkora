fe9f3942ddcba8c7e0072b967f20d9ce91707993
Ashish Modak <toashish2006@gmail.com>
2026-07-28 19:59:18 +0530

    initialized the view model for user authentication activities

--- app/src/main/java/com/example/notesapp/viewmodel/AuthViewModel.kt
+++ app/src/main/java/com/example/notesapp/viewmodel/AuthViewModel.kt
@@ -1,2 +1,29 @@
 package com.example.notesapp.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.notesapp.model.RegisterRequest
+import com.example.notesapp.model.RegisterResponse
+import com.example.notesapp.repository.AuthRepository
+import kotlinx.coroutines.launch
+import retrofit2.Response
+
+
+class AuthViewModel: ViewModel() {
+
+    private val repository = AuthRepository()
+
+    private val _registerResponse = MutableLiveData<Response<RegisterResponse>>()
+    val registerResponse: LiveData<Response<RegisterResponse>>
+        get() = _registerResponse
+
+    fun registerUser(request: RegisterRequest) {
+
+        viewModelScope.launch {
+
+            _registerResponse.value = repository.registerUser((request))
+        }
+    }
+}
