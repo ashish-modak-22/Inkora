c16c1478c5da942f4c4bd0d3a5cbc7952c3e8645
Ashish Modak <toashish2006@gmail.com>
2026-07-28 15:33:40 +0530

    Created retrofit instance file

--- app/src/main/java/com/example/notesapp/api/RetrofitInstance.kt
+++ app/src/main/java/com/example/notesapp/api/RetrofitInstance.kt
@@ -1,2 +1,20 @@
 package com.example.notesapp.api
 
+import com.example.notesapp.utils.Constants
+import com.example.notesapp.api.ApiService
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+
+object RetrofitInstance {
+
+    val api: ApiService by lazy {
+
+        // The Retrofit.Builder() works similar to that of the app = FastAPI()
+        Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(ApiService::class.java)
+    }
+}
