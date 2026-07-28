1d8f5c71942f72d3fddd0d228adc8e49baa67b96
Ashish Modak <toashish2006@gmail.com>
2026-07-28 19:33:19 +0530

    created repository for user registration

--- app/src/main/java/com/example/notesapp/repository/AuthRepository.kt
+++ app/src/main/java/com/example/notesapp/repository/AuthRepository.kt
@@ -1,2 +1,16 @@
 package com.example.notesapp.repository
 
+import com.example.notesapp.api.RetrofitInstance
+import com.example.notesapp.model.RegisterRequest
+import com.example.notesapp.model.RegisterResponse
+import retrofit2.Response
+
+class AuthRepository {
+
+    suspend fun registerUser(
+        request: RegisterRequest
+    ): Response<RegisterResponse> {
+
+        return RetrofitInstance.api.registerUser(request)
+    }
+}
