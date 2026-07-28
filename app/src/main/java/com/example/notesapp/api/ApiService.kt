d7718c0ad382725e9d057697828de767f39a8d64
Ashish Modak <toashish2006@gmail.com>
2026-07-28 19:00:22 +0530

    created request api for user registration and response given after registration

--- app/src/main/java/com/example/notesapp/api/ApiService.kt
+++ app/src/main/java/com/example/notesapp/api/ApiService.kt
@@ -1,2 +1,16 @@
 package com.example.notesapp.api
 
+import com.example.notesapp.model.RegisterRequest
+import com.example.notesapp.model.RegisterResponse
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+
+interface ApiService {
+
+    @POST("/auth/register")
+    suspend fun registerUser(
+        @Body request: RegisterRequest
+    ): Response<RegisterResponse>
+}
