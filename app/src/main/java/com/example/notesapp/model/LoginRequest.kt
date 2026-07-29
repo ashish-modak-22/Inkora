e3632a9b26d7084b199d10c80093ad87292e99f5
Ashish Modak <toashish2006@gmail.com>
2026-07-29 22:45:32 +0530

    created LoginRequest.kt file to handle the user request for login

--- app/src/main/java/com/example/notesapp/model/LoginRequest.kt
+++ app/src/main/java/com/example/notesapp/model/LoginRequest.kt
@@ -1,2 +1,12 @@
+/*
+* This file represents the request body that android will send to the FastAPI backend during user login
+*/
+
+
 package com.example.notesapp.model
 
+data class LoginRequest (
+
+    val email: String,
+    val password: String
+)
