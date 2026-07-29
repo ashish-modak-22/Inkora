15d2932b140481b77275e8a64494c5adf60caf58
Ashish Modak <toashish2006@gmail.com>
2026-07-29 23:15:36 +0530

    added login api inside the ApiService.kt file

--- app/src/main/java/com/example/notesapp/api/ApiService.kt
+++ app/src/main/java/com/example/notesapp/api/ApiService.kt
@@ -1,7 +1,15 @@
+/*
+* This file is going to define all backend API endpoints used by the Android apk
+* This file describes how to communicate with the backend
+*/
+
+
 package com.example.notesapp.api
 
 import com.example.notesapp.model.RegisterRequest
 import com.example.notesapp.model.RegisterResponse
+import com.example.notesapp.model.LoginRequest
+import com.example.notesapp.model.LoginResponse
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.POST
@@ -13,4 +21,9 @@ interface ApiService {
     suspend fun registerUser(
         @Body request: RegisterRequest
     ): Response<RegisterResponse>
+
+    @POST("/auth/login")
+    suspend fun loginUser(
+        @Body request: LoginRequest
+    ): Response<LoginResponse>
 }
