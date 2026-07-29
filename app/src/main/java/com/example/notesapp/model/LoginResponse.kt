091fbfe0cbcfae1b309dd49ff2cdce274d0345d3
Ashish Modak <toashish2006@gmail.com>
2026-07-29 22:49:48 +0530

    created the LoginResponse.kt file that will return the JWT access token and token type after successful login

--- app/src/main/java/com/example/notesapp/model/LoginResponse.kt
+++ app/src/main/java/com/example/notesapp/model/LoginResponse.kt
@@ -1,2 +1,12 @@
+/*
+* This data class file will return the JWT token after successful login
+*/
+
+
 package com.example.notesapp.model
 
+data class LoginResponse (
+
+    val accessToken: String,
+    val tokenType: String
+)
