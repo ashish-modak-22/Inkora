e379822037eeb77b01d301a7a403994d999e134b
Ashish Modak <toashish2006@gmail.com>
2026-07-30 12:51:58 +0530

    added the save token function to save the returned jwt token

--- app/src/main/java/com/example/notesapp/datastore/TokenManager.kt
+++ app/src/main/java/com/example/notesapp/datastore/TokenManager.kt
@@ -8,14 +8,23 @@ package com.example.notesapp.datastore
 import android.content.Context
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.preferencesDataStore
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
 
 
 private val Context.dataStore by preferencesDataStore(
     name = "user_preferences"
 )
 
-class TokenManager(
-    private val context: Context
-) {
+class TokenManager(private val context: Context) {
 
+    // Preference key used to store the JWT token
+    private val TOKEN_KEY = stringPreferencesKey("jwt_token")
+
+    // With this function, the JWT token will be saved into DataStore
+    suspend fun saveToken(token: String){
+        context.dataStore.edit { preferences ->
+            preferences[TOKEN_KEY] = token
+        }
+    }
 }
