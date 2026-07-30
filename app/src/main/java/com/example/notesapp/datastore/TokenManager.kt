0b45696d73913dcbcf5a697cfbb6ec4ada1ee87a
Ashish Modak <toashish2006@gmail.com>
2026-07-30 12:36:50 +0530

    created the TokenManager.kt file that will be used for datastore

--- app/src/main/java/com/example/notesapp/datastore/TokenManager.kt
+++ app/src/main/java/com/example/notesapp/datastore/TokenManager.kt
@@ -1,2 +1,21 @@
+/*
+*  This class is responsible for managing the JWT token using Jetpack Datastore inside the Android app.
+*/
+
+
 package com.example.notesapp.datastore
 
+import android.content.Context
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
+
+
+private val Context.dataStore by preferencesDataStore(
+    name = "user_preferences"
+)
+
+class TokenManager(
+    private val context: Context
+) {
+
+}
