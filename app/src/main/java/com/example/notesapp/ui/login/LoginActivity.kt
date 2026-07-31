0ed33554fc211f186cfd577e450efdc33456a029
Ashish Modak <toashish2006@gmail.com>
2026-07-31 19:32:52 +0530

    configured view binding

--- app/src/main/java/com/example/notesapp/ui/login/LoginActivity.kt
+++ app/src/main/java/com/example/notesapp/ui/login/LoginActivity.kt
@@ -1,16 +1,18 @@
 package com.example.notesapp.ui.login
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.notesapp.R
+import com.example.notesapp.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
     }
 }
