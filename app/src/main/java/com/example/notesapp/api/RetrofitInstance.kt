771c584faa0c4875743f74da37b577a4fd314e58
Ashish Modak <toashish2006@gmail.com>
2026-08-01 12:49:18 +0530

    AndroidManifest.xml : changed the launcher activity from MainActivity to LoginActivity

--- app/src/main/java/com/example/notesapp/api/RetrofitInstance.kt
+++ app/src/main/java/com/example/notesapp/api/RetrofitInstance.kt
@@ -1,33 +1,32 @@
-package com.example.notesapp.api
-
-import com.example.notesapp.utils.Constants
-import com.example.notesapp.api.ApiService
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
-import android.content.Context
-import com.example.notesapp.interceptor.AuthInterceptor
-import okhttp3.OkHttpClient
-
-
-object RetrofitInstance {
-
-    fun getApi(context: Context): ApiService {
-
-        // The Retrofit.Builder() works similar to that of the app = FastAPI()
-        return Retrofit.Builder()
-            .baseUrl(Constants.BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
-            .client(getClient(context))
-            .build()
-            .create(ApiService::class.java)
-    }
-
-    // The HTTP client used by retrofit
-    private fun getClient(context: Context): OkHttpClient {
-
-        return OkHttpClient.Builder()
-            .addInterceptor (
-                AuthInterceptor(context)
-                ).build()
-    }
-}
+//package com.example.notesapp.api
+//
+//import com.example.notesapp.utils.Constants
+//import com.example.notesapp.api.ApiService
+//import retrofit2.Retrofit
+//import retrofit2.converter.gson.GsonConverterFactory
+//import android.content.Context
+//import okhttp3.OkHttpClient
+//
+//
+//object RetrofitInstance {
+//
+//    fun getApi(context: Context): ApiService {
+//
+//        // The Retrofit.Builder() works similar to that of the app = FastAPI()
+//        return Retrofit.Builder()
+//            .baseUrl(Constants.BASE_URL)
+//            .addConverterFactory(GsonConverterFactory.create())
+//            .client(getClient(context))
+//            .build()
+//            .create(ApiService::class.java)
+//    }
+//
+//    // The HTTP client used by retrofit
+//    private fun getClient(context: Context): OkHttpClient {
+//
+//        return OkHttpClient.Builder()
+//            .addInterceptor (
+//                AuthInterceptor(context)
+//                ).build()
+//    }
+//}
--- app/src/main/java/com/example/notesapp/repository/AuthRepository.kt
+++ app/src/main/java/com/example/notesapp/repository/AuthRepository.kt
@@ -1,29 +1,29 @@
-/*
-* This repository is responsible for all authentication related API operations
-*/
-
-
-package com.example.notesapp.repository
-
-import android.content.Context
-import com.example.notesapp.api.RetrofitInstance
-import com.example.notesapp.model.RegisterRequest
-import com.example.notesapp.model.RegisterResponse
-import com.example.notesapp.model.LoginRequest
-import com.example.notesapp.model.LoginResponse
-import retrofit2.Response
-
-class AuthRepository(private val context: Context) {
-
-    suspend fun registerUser(
-        request: RegisterRequest
-    ): Response<RegisterResponse> {
-        return RetrofitInstance.getApi(context).registerUser(request)
-    }
-
-    suspend fun loginUser(
-        request: LoginRequest
-    ): Response<LoginResponse> {
-        return RetrofitInstance.getApi(context).loginUser(request)
-    }
-}
+///*
+//* This repository is responsible for all authentication related API operations
+//*/
+//
+//
+//package com.example.notesapp.repository
+//
+//import android.content.Context
+//import com.example.notesapp.api.RetrofitInstance
+//import com.example.notesapp.model.RegisterRequest
+//import com.example.notesapp.model.RegisterResponse
+//import com.example.notesapp.model.LoginRequest
+//import com.example.notesapp.model.LoginResponse
+//import retrofit2.Response
+//
+//class AuthRepository(private val context: Context) {
+//
+//    suspend fun registerUser(
+//        request: RegisterRequest
+//    ): Response<RegisterResponse> {
+//        return RetrofitInstance.getApi(context).registerUser(request)
+//    }
+//
+//    suspend fun loginUser(
+//        request: LoginRequest
+//    ): Response<LoginResponse> {
+//        return RetrofitInstance.getApi(context).loginUser(request)
+//    }
+//}
