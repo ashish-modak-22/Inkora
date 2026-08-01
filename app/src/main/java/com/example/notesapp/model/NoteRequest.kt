098deb99cba2f92d311ac5b30d2b11cf06e75104
Ashish Modak <toashish2006@gmail.com>
2026-08-01 14:15:23 +0530

    Created data class for a new note

--- app/src/main/java/com/example/notesapp/model/NoteRequest.kt
+++ app/src/main/java/com/example/notesapp/model/NoteRequest.kt
@@ -1,2 +1,12 @@
 package com.example.notesapp.model
 
+import com.google.gson.annotations.SerializedName
+
+data class NoteRequest(
+
+    @SerializedName("title")
+    val title: String,
+
+    @SerializedName("content")
+    val content: String
+)
