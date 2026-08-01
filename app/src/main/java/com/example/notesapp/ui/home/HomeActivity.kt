56cacfc5ab2e1aa3f64118ef604eef3c36eb98cf
Ashish Modak <toashish2006@gmail.com>
2026-08-01 13:09:09 +0530

    configured recycler view in HomeActivity.kt with fake notes

--- app/src/main/java/com/example/notesapp/ui/home/HomeActivity.kt
+++ app/src/main/java/com/example/notesapp/ui/home/HomeActivity.kt
@@ -2,12 +2,31 @@ package com.example.notesapp.ui.home
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.notesapp.R
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.notesapp.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
 
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val noteList = listOf(
+            "Learn Kotlin",
+            "FastAPI Backend",
+            "MVVM Structure",
+            "Prepare for SIH",
+            "DSA dynamic programming",
+            "Improve the UI/UX"
+        )
+
+        val adapter = NoteAdapter(noteList)
+
+        binding.rvNotes.layoutManager = LinearLayoutManager(this)
+        binding.rvNotes.adapter = adapter
     }
 }
