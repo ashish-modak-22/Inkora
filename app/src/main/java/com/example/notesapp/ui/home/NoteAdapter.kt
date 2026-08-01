7de63af84e98175da6663b6cf9ede081ff585373
Ashish Modak <toashish2006@gmail.com>
2026-08-01 10:48:02 +0530

    created adapter for note list

--- app/src/main/java/com/example/notesapp/ui/home/NoteAdapter.kt
+++ app/src/main/java/com/example/notesapp/ui/home/NoteAdapter.kt
@@ -1,2 +1,39 @@
 package com.example.notesapp.ui.home
 
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.notesapp.databinding.ItemNoteBinding
+
+class NoteAdapter(
+    private val noteList: List<String>
+): RecyclerView.Adapter<NoteAdapter.NoteViewHolder>() {
+
+    inner class NoteViewHolder(
+        private val binding: ItemNoteBinding
+    ): RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(note: String) {
+            binding.tvTitle.text = note
+            binding.tvDescription.text = "This is a sample description."
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
+        val binding = ItemNoteBinding.inflate(LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+
+        return NoteViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
+        holder.bind(noteList[position])
+    }
+
+    override fun getItemCount(): Int {
+        return noteList.size
+    }
+}
